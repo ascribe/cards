cb96c4c41a9a226667e03f1ee5f8b14538d6b0fb
troymc <troy@ascribe.io>
2015-11-03 17:20:38 +0100

    Moved within 79 char width

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ from setuptools import setup
 setup(
     name='cards',
     version='0.1',
-    description="Server to respond to Facebook's crawler, Twitter's crawler, etc.",
+    description="Server to respond to Facebook's crawler, " +
+                "Twitter's crawler, etc.",
     author='Troy McConaghy',
     author_email='troy@ascribe.io',
     packages=['ascribe_cards'],
